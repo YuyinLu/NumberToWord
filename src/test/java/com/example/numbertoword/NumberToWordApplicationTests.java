e4dd89aba664156cba0f63b6c41a639b1a3f0500
雨茵 陆 <yinlu@yuyindeMacBook-Air.local>
2023-10-19 18:06:52 +0200

    Initial commit

---
+++ src/test/java/com/example/numbertoword/NumberToWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.numbertoword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberToWordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
